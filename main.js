afd357322b684300291b22092cc5262b166c38cf
Katie Schmidt <katie_schmidt7@icloud.com>
2019-03-11 13:58:48 -0400

    create navigation

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("connected");
+const navigation = document.querySelector(".navigation");
+const openButton = document.querySelector(".nav-button-open");
+
+const closeButton = document.querySelector(".nav-button-close");
+
+const navItems = document.querySelectorAll(".navigation__list-item");
+
+openButton.onclick = () => {
+  navigation.classList.remove("navigation-hidden");
+};
+
+closeButton.onclick = () => {
+  navigation.classList.add("navigation-hidden");
+};
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].onclick = function(e) {
+    navigation.classList.add("navigation-hidden");
+  };
+}
